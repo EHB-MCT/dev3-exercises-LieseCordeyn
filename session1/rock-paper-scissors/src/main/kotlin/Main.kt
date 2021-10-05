0e295158a2a669e320fa3320a9bb809baf3da620
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-10-05 12:29:32 +0200

    finished rock paper scissor

--- session1/rock-paper-scissors/src/main/kotlin/Main.kt
+++ session1/rock-paper-scissors/src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ fun main(){
 
     var userInput = readLine()
 
-    if (optionComputer == "rock"){
+    /* if (optionComputer == "rock"){
         if (userInput == "rock"){
             println("draw")
         } else if (userInput == "paper"){
@@ -13,6 +13,31 @@ fun main(){
         } else if (userInput == "scissors"){
             println("you lose")
         }
+    } */
+
+    if (userInput == "paper"){
+        when (optionComputer){
+            "paper" -> println("draw")
+            "scissors" -> println("you lose")
+            "rock" -> println("you win")
+        }
     }
+    if (userInput == "scissors"){
+        when (optionComputer){
+            "paper" -> println("you win")
+            "scissors" -> println("draw")
+            "rock" -> println("you lose")
+        }
+    }
+
+    if (userInput == "rock"){
+        when (optionComputer){
+            "paper" -> println("you lose")
+            "scissors" -> println("you win")
+            "rock" -> println("draw")
+        }
+    }
+
+    println( "Computer chose $optionComputer");
 
 }
