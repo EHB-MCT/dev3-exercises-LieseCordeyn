450e536bb4ce4a5cad60200c266c9ae8e5131176
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-10-12 10:51:16 +0200

    made challenge one

--- session2/adventureGame/src/main/kotlin/Main.kt
+++ session2/adventureGame/src/main/kotlin/Main.kt
@@ -1,9 +1,30 @@
 fun main(){
-    println("Welcome")
-    println("Let's start the game")
+    println("Ready to start your day?")
     challengeOne()
 }
 
 fun challengeOne(){
-    println("Challenge One:")
+    println("It's 7am, your alarm clock is making a terrible noise")
+    println("How many times do you snooze?")
+
+    val rightAnswers = setOf<String>("none", "one time", "two times", "three times")
+
+    val userInput = readLine()
+
+    if (rightAnswers.contains(userInput)){
+        println("Yes!! You managed to wake up in time")
+        challengeTwo()
+    } else {
+        gameOver()
+    }
+}
+
+fun challengeTwo(){
+
+}
+
+fun gameOver(){
+    println("Too bad, you fell asleep again")
+    println("ready to try again?")
+    challengeOne()
 }
