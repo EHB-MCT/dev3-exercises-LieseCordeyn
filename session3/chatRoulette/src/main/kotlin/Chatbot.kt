f3970a838d7e17f22cdb080da7427f85dc87e836
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-10-19 12:00:01 +0200

    changed part 3 with new theory

--- session3/chatRoulette/src/main/kotlin/Chatbot.kt
+++ session3/chatRoulette/src/main/kotlin/Chatbot.kt
@@ -1,5 +1,8 @@
-class Chatbot(override val name:String): Person(name) {
-    fun chat(){
-        println("beep beep I'm a sheep")
+class Chatbot(val name: String): ChatPartner {
+   override fun introduce(){
+        println("Hi, my name is $name")
+    }
+    override fun talk(){
+        println("beep beep, I'm a sheep")
     }
 }
--- session3/chatRoulette/src/main/kotlin/Main.kt
+++ session3/chatRoulette/src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 fun main(){
-   val persons = arrayOf<Person>(Student("britt"), Teacher("Bert"), Comedian("Mike"))
-
+   val persons = arrayOf(Student("britt"), Teacher("Bert"), Comedian("Mike"), Chatbot("sheep"))
     val randomPerson = persons.random()
     randomPerson.introduce()
     randomPerson.talk()
-  /*  when(randomPerson) {
+
+
+  /* EIGEN CODE
+  when(randomPerson) {
         is Student -> randomPerson.gossip()
         is Teacher -> randomPerson.funfact()
         is Comedian -> randomPerson.joke()
