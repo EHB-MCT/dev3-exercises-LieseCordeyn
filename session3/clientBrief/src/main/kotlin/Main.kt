56a049b90b16361b7f30c95534ebbcbc9a77e844
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-10-19 09:57:31 +0200

    finished ex1

--- session3/clientBrief/src/main/kotlin/Main.kt
+++ session3/clientBrief/src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main(){
-    val design1 = Course("design1","design","Maaike", 5, 1,1,true,"Learning how to work with Adobe" )
+    val design1 = Course("design1",
+        "design",
+        "Maaike Beuten",
+        5,
+        1,
+        1,
+        true,
+        "Learning how to work with Adobe" )
+
+    val dev1 = Course("development1",
+        "development",
+        "Peter Dickx",
+        5,
+        1,
+        1,
+        true,
+        "Learning how to work with javascript" )
+    design1.name
 }
