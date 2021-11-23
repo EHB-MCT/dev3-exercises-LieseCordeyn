fe579132c05c1860557818af3d0c1472a74c0977
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-11-23 12:46:17 +0100

    finished part 2 of excersise
    added  that you only see the first three trains on this day, example with Halle

--- session6/trains/src/main/kotlin/Main.kt
+++ session6/trains/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.sql.*
+import java.time.LocalDate
 import java.util.*
 
 fun main(){
@@ -18,21 +19,26 @@ fun main(){
                 ":" + "3306" + "/" +
                 credentials.databaseName,
         connectionProps)
-
+    val currentDate = LocalDate.now();
+    println(currentDate)
     println("Waar wil je naartoe?")
     val city = readLine()
 
    //test data ophalen
-    val statement = connection.prepareStatement("SELECT * FROM 6_rides" + " " +
-            "LEFT JOIN 6_trains ON 6_trains.id = 6_rides.id_train" + " " +
-            "LEFT JOIN 6_cities ON 6_cities.id = 6_rides.arrival_city_id" + " " +
-            "WHERE city_name = '$city' LIMIT 1")
+    val statement = connection.prepareStatement("SELECT * FROM 6_rides" +
+            " LEFT JOIN 6_trains ON 6_trains.id = 6_rides.id_train" +
+            " LEFT JOIN 6_cities ON 6_cities.id = 6_rides.arrival_city_id" +
+            " WHERE city_name = '$city' AND departure_time LIKE '$currentDate%'" +
+            " ORDER BY (departure_time) ASC" +
+            " LIMIT 3")
     val result = statement.executeQuery()
 
     while(result.next()) {
         println( "Naar:" + result.getString("city_name"))
         println( "Om:" + result.getString("departure_time"))
+        println("Op perron " + result.getInt("platform"))
         println("Met:" + result.getString("name"))
+        println(" ")
     }
 
 
