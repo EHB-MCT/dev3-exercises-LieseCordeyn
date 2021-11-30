3251a63c161b2d035f0092aa57668ba427c22011
LieseCordeyn <liese.cordeyn@student.ehb.be>
2021-11-30 10:39:12 +0100

    setUp words and classes

---
+++ session7/duolingo/src/main/kotlin/Duolingo.kt
@@ -0,0 +1,27 @@
+class Duolingo {
+ init{
+     val words = arrayOf(Word("cat", "kat", "Engels"),
+                         Word("chat", "kat","Frans"),
+                         Word("dog", "hond","Engels"),
+                         Word("chien", "hond","Frans"),
+                         Word("bunny", "konijn","Engels"),
+                         Word("lapin", "konijn","Frans"),
+                         Word("fish", "vis","Engels"),
+                         Word("poisson", "vis","Frans"),
+                         Word("horse", "paard","Engels"),
+                         Word("cheval", "paard","Frans"),
+                         Word("goat", "geit","Engels"),
+                         Word("chevre", "geit","Frans"),
+                         Word("chicken", "kip","Engels"),
+                         Word("poulet", "kip","Frans"),
+                         Word("mouse", "muis","Engels"),
+                         Word("souris", "muis","Frans"),
+                         Word("donkey", "ezel","Engels"),
+                         Word("ane", "ezel","Frans"),
+                         Word("snake", "slang","Engels"),
+                         Word("serpent", "slang","Frans"),
+
+     )
+ }
+}
+
